c035c294f098233aa36a92f7ce117a5d305cdb23
Luan Melo <luanmelolima@gmail.com>
2024-08-28 20:26:53 -0300

    Estilziando a parte central

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "./components/ui/separator";
 import { Button } from "./components/ui/button";
 import { Github } from "lucide-react";
 
@@ -11,12 +12,28 @@ export function App() {
           <span className="text-sm text-muted-foreground">
             Desenvolvido com 💜 no NLW da Rocketseat
           </span>
+          
+          <Separator orientation='vertical' className="h-6" />
+
           <Button variant="outline">
             <Github />
             Github
           </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+
+          </div>
+
+          <p className="text-small text-muted--foreground">
+            Lembre-se: você pode utilizar a variável transcription no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+      <aside className="w-80"></aside>  
+      </main>
     </div>
   )
 }
