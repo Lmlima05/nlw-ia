2071f138d1f890a3296c6b682722ba94aba50334
Luan Melo <luanmelolima@gmail.com>
2024-09-03 21:37:47 -0300

    Ajustando o Textarea

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -26,8 +26,14 @@ export function App() {
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
           <div className="grid grid-rows-2 gap-4 flex-1">
-            <Textarea />
-            <Textarea />
+            <Textarea 
+              className="resize-none p-4"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea 
+              className="resize-none p-4"
+              placeholder="Resultado gerado pela IA..." readOnly
+            />
           </div>
 
           <p className="text-small text-muted--foreground">
